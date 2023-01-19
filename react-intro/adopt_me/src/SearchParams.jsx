bfe0fd9e2ab7820b7141cd6756224e2cb8296711
mrance21 <mrance21@gmail.com>
2023-01-18 22:56:40 -0500

    modified SearchParam

--- react-intro/adopt_me/src/SearchParams.jsx
+++ react-intro/adopt_me/src/SearchParams.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import { useQuery } from "@tanstack/react-query"
+import AdoptedPetContext from "./AdoptedPetContext"
 import Results from "./Results"
 import fetchSearch from "./fetchSearch"
 import useBreedList from "./useBreedList"
@@ -14,7 +15,8 @@ const SearchParams = () => {
   })
   const [animal, setAnimal] = useState('')
   const [breeds] = useBreedList(animal)
-  
+  const [adoptedPet] = useContext(AdoptedPetContext)
+
   const results = useQuery(["search", requestParams], fetchSearch)
   const pets = results?.data?.pets ?? []
 
@@ -30,6 +32,13 @@ const SearchParams = () => {
         }
         setRequestParams(obj)
       }}>
+        {
+          adoptedPet ? (
+            <div className="pet image-container">
+              <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+            </div>
+          ) : null
+        }
         <label htmlFor="location">
           Location
           <input 
