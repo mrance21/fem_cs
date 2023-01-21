d55ce34d22394d3cef59741b03605da042219f7a
mrance21 <mrance21@gmail.com>
2023-01-21 16:22:38 -0500

    added Suspense and lazy loading

--- react-intro/adopt_me/src/App.jsx
+++ react-intro/adopt_me/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
+
+const Details = lazy(() => import('./Details'))
+const SearchParams = lazy(() => import('./SearchParams'))
 
 const queryClient = new QueryClient ({
   defaultOptions: {
@@ -20,8 +21,15 @@ const App = () => {
   return (
     <div className="p-0 m-0" style={{ background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)" }}>
       <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <AdoptedPetContext.Provider value={adoptedPet}>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <QueryClientProvider client={queryClient}>
+            <Suspense 
+              fallback={
+                <div className="loading-pane">
+                  <h2 className="loader">🐶</h2>
+                </div>
+              }
+            >
             <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 
             via-orange-500 to-red-500 p-7 text-center">
               <Link className="text-6xl text-white hover:text-gray-200" to="/">Adopt Me</Link>
@@ -30,8 +38,9 @@ const App = () => {
               <Route path="/details/:id" element={<Details />} />
               <Route path="/" element={<SearchParams />}/>
             </Routes>
-          </AdoptedPetContext.Provider>
-        </QueryClientProvider>
+            </Suspense>
+          </QueryClientProvider>
+        </AdoptedPetContext.Provider>
       </BrowserRouter>
     </div>
   )
