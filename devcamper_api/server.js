6ce74cc9fdd967328355c5e2dbf9ee85b033db13
mrance21 <mrance21@gmail.com>
2023-01-30 17:12:59 -0500

    added model

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
+const mongoose = require('mongoose');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
+
+//connect to db
+mongoose.set('strictQuery', true);
+connectDB();
+
+// route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -20,6 +26,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`server is running in ${process.env.NODE_ENV} on port ${PORT}`);
+});
+
+//handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //close server and exit process
+  server.close(() => process.exit(1));
 });
