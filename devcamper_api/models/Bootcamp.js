b97869e4c08dc478c4a874834c4e6b3e1d88d6bd
mrance21 <mrance21@gmail.com>
2023-01-30 21:14:46 -0500

    added logic to controllers

--- devcamper_api/models/Bootcamp.js
+++ devcamper_api/models/Bootcamp.js
@@ -6,13 +6,13 @@ const BootcampSchema = new mongoose.Schema({
     required: [true, 'Please add a name'],
     unique: true,
     trim: true,
-    maxLength: [50, 'Name cannot be more than 50 characters'],
+    maxlength: [50, 'Name can not be more than 50 characters'],
   },
   slug: String,
   description: {
     type: String,
-    required: [true, 'Please add a name'],
-    maxLength: [50, 'Desription cannot be more than 50 characters'],
+    required: [true, 'Please add a description'],
+    maxlength: [500, 'Description can not be more than 500 characters'],
   },
   website: {
     type: String,
@@ -23,12 +23,13 @@ const BootcampSchema = new mongoose.Schema({
   },
   phone: {
     type: String,
-    maxLength: [20, 'Phone number can not be longer than 20 characters'],
+    maxlength: [20, 'Phone number can not be longer than 20 characters'],
   },
   email: {
     type: String,
     match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email',
     ],
   },
   address: {
@@ -36,15 +37,13 @@ const BootcampSchema = new mongoose.Schema({
     required: [true, 'Please add an address'],
   },
   location: {
-    //GeoJSON Point
+    // GeoJSON Point
     type: {
-      type: String, // Don't do `{ location: { type: String } }`
-      enum: ['Point'], // 'location.type' must be 'Point'
-      required: true,
+      type: String,
+      enum: ['Point'],
     },
     coordinates: {
       type: [Number],
-      required: true,
       index: '2dsphere',
     },
     formattedAddress: String,
@@ -55,7 +54,7 @@ const BootcampSchema = new mongoose.Schema({
     country: String,
   },
   careers: {
-    //Array of strings
+    // Array of strings
     type: [String],
     required: true,
     enum: [
@@ -70,7 +69,7 @@ const BootcampSchema = new mongoose.Schema({
   averageRating: {
     type: Number,
     min: [1, 'Rating must be at least 1'],
-    max: [10, 'Rating must be no more than 10'],
+    max: [10, 'Rating must can not be more than 10'],
   },
   averageCost: Number,
   photo: {
@@ -96,11 +95,6 @@ const BootcampSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now,
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: true,
   },
 });
 
