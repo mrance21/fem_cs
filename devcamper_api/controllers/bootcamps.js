166d30cdd79bd0a6e7295f0181ad933e6dc23654
mrance21 <mrance21@gmail.com>
2023-02-01 15:14:33 -0500

    app

--- devcamper_api/controllers/bootcamps.js
+++ devcamper_api/controllers/bootcamps.js
@@ -65,10 +65,18 @@ exports.updatetBootcamp = async (req, res, next) => {
 //@desc    Delete bootcamp
 //@route   DELETE /api/v1/bootcamps/:id
 //@access  Private
-exports.deleteBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: {} });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //router takes request and decides which controller will handle it, controller accepts/handles request
